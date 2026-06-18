68b27dbab74c40ee6166c55450cb9fc0087b7b8d
ShahzaibNaseem <shahzaibnaseem521@gmail.com>
2026-06-18 15:19:57 +0500

    fix: link AppBinding directly to root GetPage to resolve HomeViewModel not found runtime error

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
         GetPage(
           name: '/',
           page: () => const HomeScreen(),
+          binding: AppBinding(),
         ),
         GetPage(
           name: '/preview',
